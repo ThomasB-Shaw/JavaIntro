4864fc1a22f92c900ae478c1f8cb022af0e77aca
Thomas Brookshaw <tbrookshaw13@gmail.com>
2021-02-08 08:44:50 -0600

    added Scanner

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,15 @@
 package com.example.helloworld;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("You are " + name);
+
+        String helloWorld = "Hello World!!!";
+        System.out.println(helloWorld);
     }
 }
