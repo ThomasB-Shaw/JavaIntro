7d8b87bfe239e5f69dec41d7a15daa9a2b6df127
Thomas Brookshaw <tbrookshaw13@gmail.com>
2021-02-08 09:27:10 -0600

    Created Mortgage Calc

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,15 +1,33 @@
 package com.example.helloworld;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Name: ");
-        String name = scanner.nextLine().trim();
-        System.out.println("You are " + name);
+        final byte monthsInYear = 12;
+        final byte percent = 100;
 
-        String helloWorld = "Hello World!!!";
-        System.out.println(helloWorld);
+        Scanner pScanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = pScanner.nextInt();
+        System.out.println("Principal = " + principal);
+
+        Scanner rScanner = new Scanner(System.in);
+        System.out.print("Rate: ");
+        double rate = rScanner.nextDouble();
+        double monthlyRate = rate / percent / monthsInYear;
+        System.out.println("Rate = " + monthlyRate);
+
+        Scanner nScanner = new Scanner(System.in);
+        System.out.print("Number of Payments: ");
+        int numberPayments = nScanner.nextInt();
+        System.out.println("Number of Payments = " + numberPayments);
+
+        double mortgage = principal * ((rate*Math.pow(1+monthlyRate , numberPayments)/(Math.pow(1+monthlyRate, numberPayments))-1));
+        System.out.println("Total Mortgage Payment: " + mortgage);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Total Mortgage Payment: " + mortgageFormatted);
     }
 }
