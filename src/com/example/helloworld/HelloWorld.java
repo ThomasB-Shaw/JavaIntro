acc85283e4ec94bcac4f7dba4cb7f7306f5c2d32
Thomas Brookshaw <tbrookshaw13@gmail.com>
2021-02-08 09:31:07 -0600

    Refactored Scanner Inputs, Fixed Equation

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -8,24 +8,23 @@ public class HelloWorld {
         final byte monthsInYear = 12;
         final byte percent = 100;
 
-        Scanner pScanner = new Scanner(System.in);
+        Scanner Scanner = new Scanner(System.in);
+
         System.out.print("Principal: ");
-        int principal = pScanner.nextInt();
+        int principal = Scanner.nextInt();
         System.out.println("Principal = " + principal);
 
-        Scanner rScanner = new Scanner(System.in);
         System.out.print("Rate: ");
-        double rate = rScanner.nextDouble();
+        double rate = Scanner.nextDouble();
         double monthlyRate = rate / percent / monthsInYear;
         System.out.println("Rate = " + monthlyRate);
 
-        Scanner nScanner = new Scanner(System.in);
         System.out.print("Number of Payments: ");
-        int numberPayments = nScanner.nextInt();
+        byte years = Scanner.nextByte();
+        int numberPayments = years * monthsInYear;
         System.out.println("Number of Payments = " + numberPayments);
 
         double mortgage = principal * ((rate*Math.pow(1+monthlyRate , numberPayments)/(Math.pow(1+monthlyRate, numberPayments))-1));
-        System.out.println("Total Mortgage Payment: " + mortgage);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Total Mortgage Payment: " + mortgageFormatted);
