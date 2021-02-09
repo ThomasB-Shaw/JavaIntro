7e8f91217cfc0852cea5739bc125384ea8e7b63b
Thomas Brookshaw <tbrookshaw13@gmail.com>
2021-02-09 15:16:30 -0600

    added in quick notes to java intro repo

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        // Constants
         final byte monthsInYear = 12;
         final byte percent = 100;
 
+        // Global Variable
         int principal = 0;
         double monthlyRate = 0;
         int numberPayments = 0;
 
+        // Global Scanner used to get user input
         Scanner scanner = new Scanner(System.in);
 
+        // Principal input with while statement to ensure valid integer
         while(true) {
             System.out.print("Principal: ");
             principal = scanner.nextInt();
@@ -22,6 +26,7 @@ public class HelloWorld {
             System.out.println("Please enter a valid integer above 0");
         }
 
+        // Rate input with while statement to ensure valid double input
         while(true) {
             System.out.print("Rate: ");
             double rate = scanner.nextDouble();
@@ -31,6 +36,7 @@ public class HelloWorld {
             System.out.println("Please enter a rate between 1% and 30%");
         }
 
+        // Payment# input with while statement to prevent invalid by input
         while(true) {
             System.out.print("Number of Payments: ");
             byte years = scanner.nextByte();
@@ -40,8 +46,10 @@ public class HelloWorld {
             System.out.println("Please enter a valid integer less than 50");
         }
 
+        // Equation to get the cost of monthly mortgage
         double mortgage = principal * (monthlyRate * Math.pow(1 + monthlyRate , numberPayments)/(Math.pow(1 + monthlyRate, numberPayments) - 1));
 
+        // Formats the above equation to be readable to user
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Total Mortgage Payment: " + mortgageFormatted);
     }
